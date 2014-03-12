3a5b0762013ec1daa8bc9492a46db4dd00b8e128
Dmitry Ponyatov <dponyatov@gmail.com>
2014-03-12 21:57:16 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- popup.js
+++ popup.js
@@ -1,5 +1,21 @@
 
 var USER = 'dponyatov';
-var SERVER = '127.0.0.1';
-var PORT = 1979;
+var SERVER = '127.0.0.1:1979';
 
+function lds() {
+    var plain = document.createElement("iframe");
+    plain.name='ya';
+    //plain.src='http://ya.ru';
+    document.body.appendChild(plain);
+    var img = document.createElement("img"); 
+    img.src="icon-quality-collaboration.png";
+    img.scale='50%';
+    img.style.border=1;
+    document.body.appendChild(img);
+    //var us = document.getElementById("user");
+    us=USER;
+//   window.location.assign('http://ya.ru');
+    //document.body='ya!';
+}
+
+window.onload = lds;
